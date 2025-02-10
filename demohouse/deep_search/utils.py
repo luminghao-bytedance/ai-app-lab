a72649cbe3de5562b3d5c325a0372c453c97d96b
liuminghao <liuminghao.lm@bytedance.com>
2025-02-10 15:40:12 +0800

    temp: for eval

--- demohouse/deep_search/utils.py
+++ demohouse/deep_search/utils.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 from typing import List
 
-from arkitect.core.component.llm.model import ArkMessage
+from arkitect.core.component.llm.model import ArkMessage, ArkChatCompletionChunk
+
+
+def cast_content_to_reasoning_content(chunk: ArkChatCompletionChunk) -> ArkChatCompletionChunk:
+    new_chunk = ArkChatCompletionChunk(**chunk.__dict__)
+    new_chunk.choices[0].delta.reasoning_content = chunk.choices[0].delta.content
+    new_chunk.choices[0].delta.content = ""
+    return new_chunk
 
 
 def get_last_message(messages: List[ArkMessage], role: str):
