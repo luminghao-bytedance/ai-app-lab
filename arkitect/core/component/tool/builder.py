453f6bcd2ab4c4251853b02eb5abf2cfce0d2312
wgzesg-bd <zesong.wang1@bytedance.com>
2025-03-20 16:39:34 +0800

    feat[demohouse/deepresearch]: return dict

--- arkitect/core/component/tool/builder.py
+++ arkitect/core/component/tool/builder.py
@@ -17,7 +17,7 @@ import json
 from arkitect.core.component.tool.mcp_client import MCPClient
 
 
-def build_mcp_clients_from_config(config_file: str) -> list[MCPClient]:
+def build_mcp_clients_from_config(config_file: str) -> dict[str, MCPClient]:
     # check file exist
     if not os.path.exists(config_file):
         raise ValueError(f"Config file {config_file} does not exist")
@@ -25,10 +25,10 @@ def build_mcp_clients_from_config(config_file: str) -> list[MCPClient]:
     with open(config_file, "r") as f:
         config = json.loads(f.read())
     mcp_servers_config = config.get("mcpServers", {})
-    mcp_clients = []
+    mcp_clients = {}
     for server_name in mcp_servers_config:
         command = mcp_servers_config[server_name].get("command", "")
         args = mcp_servers_config[server_name].get("args", [])
         client = MCPClient(command=command, arguments=args)
-        mcp_clients.append(client)
+        mcp_clients[server_name] = client
     return mcp_clients
